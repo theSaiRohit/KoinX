de15a4e319e8783322a4e84bc6a3e2284fdbbce5
Sai Rohit <sairohit11.s@gmail.com>
2024-03-07 22:36:05 +0530

    Project Done :)

--- styles/global-styles.ts
+++ styles/global-styles.ts
@@ -22,31 +22,10 @@ export const globalStylesCss = css`
     --aside-width: 400px;
     --padding-normal: 50px;
 
-    //
-    --font-size-small: 1.2rem;
-    --font-size-large: 6.25vw;
-    --font-size-med: 2.6rem;
-    --font-size-icon: 1.6rem;
-
-    --padding-page-dw: 10vh;
-    --padding-page-mw: 5vh;
-    --footer-grid-col-width: 275px;
-    --banner-img-height: 350px;
-    --line-height-small: 1.5em;
-    --line-height-large: calc(var(--scrollbar-width) + var(--padding-mid));
-    --carosual-mid-width: 500px;
-
     --color-bg: #eff2f5;
     --color-bg-white: #ffffff;
     --color-text-default: #111111;
     --color-off-gray: hsla(0, 0%, 63%, 0.25);
-
-    //
-    --color-bg-primary: #1d892c;
-    --color-bg-secondary: #ffd93d;
-    --color-bg-primary-light: rgb(94, 184, 129);
-    --color-nav-bg: rgba(29, 137, 44, 0.4);
-    --color-light-gray: rgba(62, 62, 62, 0.4);
   }
   * {
     margin: 0;
